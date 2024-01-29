733e99216b3ab640bb5930a046cab47a07347493
(Student  C00301768) Eliska Vrzalova <C00301768@setu.ie>
2024-01-29 11:07:45 +0000

    modifs started

--- lab10/lab10/q6.cpp
+++ lab10/lab10/q6.cpp
@@ -1,12 +1,13 @@
 
+#include <iostream>
+#include <string>
+
 // The following variables are GLOBAL variables
 const int MAX_PLAYERS = 8;	// maximum number of players
 int noOfPlayers = 8;	// the number of active players 
 std::string namesArray[MAX_PLAYERS];
 int healthArray[MAX_PLAYERS];
 
-#include <iostream>
-#include <string>
 
 int calculateSum();
 float calculateAvg();
@@ -15,11 +16,16 @@ void displayReversePlayers();
 void findHighestHealth();
 void findLowestHealth();
 
+void initializeArrays();
+
+
 int main()
 {
 	int sum = 0;
 	float average = 0.0f;
 	//function calls
+
+	initializeArrays();
 	sum = calculateSum();
 	std::cout << "The sum of players health in array:" << sum << std::endl;
 	average = calculateAvg();
@@ -29,9 +35,12 @@ int main()
 	findHighestHealth();
 	findLowestHealth();
 
+
+
 	system("pause");
 	return 0;
 }
+
 void initializeArrays()
 // Initialize the two arrays
 {
@@ -51,8 +60,9 @@ void initializeArrays()
 	healthArray[4] = 4;
 	healthArray[5] = 6;
 	healthArray[6] = 1;
-	healthArray[7] = 6;
+	healthArray[7] = 5;
 }
+
 int calculateSum()
 // calculates the sum of players health in the array 
 {
@@ -64,6 +74,7 @@ int calculateSum()
 	return sum;
 }
 float calculateAvg()
+//// calculates the average of the players health in the array 
 {
 	float sum = 0.0f;
 	float average = 0.0f;
@@ -74,3 +85,81 @@ float calculateAvg()
 	average = sum / MAX_PLAYERS;
 	return average;
 }
+void displayPlayers()
+// displays the players names and their associated health values to the screen in sequential order within the function.
+{
+	std::string message = "";
+	for (int count = 0; count < MAX_PLAYERS; count++)
+	{
+		message  = message + " The player " + namesArray[count] + " has health of " + std::to_string(healthArray[count]);
+	}
+	std::cout << message << std::endl;
+}
+
+void displayReversePlayers()
+// displays the names and their associated health values in reverse within the function. 
+{
+	std::string message = "";
+	for (int count = MAX_PLAYERS -1; count >= 0; count--)
+	{
+		message = " The player " + namesArray[count] + " has health of " + std::to_string(healthArray[count]);
+	}
+	std::cout << message << std::endl;
+}
+
+void findHighestHealth()
+// finds the player with the highest health in the array and outputs the name of the player and their highest health within the function. 
+// If there is more than one player with the highest health, output only the last player’s name.
+{
+	int highestHealth = 0;
+	std::string message = "";
+	int position = 0;
+	int count = 0;
+
+	for (count; count < MAX_PLAYERS; count++)
+	{
+		if (highestHealth < healthArray[count])
+		{
+			highestHealth = healthArray[count];
+			position = count;
+
+		}
+		if (highestHealth == healthArray[count])
+		{
+			position = count;
+		}
+		
+	}
+
+	message = message + " Player " + namesArray[position] + " health " + std::to_string(highestHealth);
+	std::cout << message << std::endl;
+}
+
+void findLowestHealth()
+// finds the player with the lowest health in the array and outputs the name of the player and their lowest health within the function. 
+//if there is more than one player with the lowest health, output only the first player’s name.
+{
+	int lowestHealth = healthArray[0];
+	std::string message = "";
+
+	for (int count = 0; count < MAX_PLAYERS; count++) // finds the lowest health
+	{
+
+		if (lowestHealth > healthArray[count])
+		{
+			lowestHealth = healthArray[count];
+		}
+	}
+	int count = 0;
+	for (count = 0; count < MAX_PLAYERS; count++) // finds the first position of the lowest health found in the previous loop
+	{
+		if (lowestHealth == healthArray[count])
+		{
+			break;
+		}
+	}
+	message = message + " Player " + namesArray[count] + " health " + std::to_string(lowestHealth);
+	std::cout << message << std::endl;
+}
+
+
